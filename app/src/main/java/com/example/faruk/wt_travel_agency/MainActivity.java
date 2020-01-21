37b072fa2b5cfa387650e640924e9195a13f2ba8
Faruk Ohran <faruk.ohran@sleepnumber.com>
2020-01-21 14:37:30 +0100

    dodan klik na drawer items

--- app/src/main/java/com/example/faruk/wt_travel_agency/MainActivity.java
+++ app/src/main/java/com/example/faruk/wt_travel_agency/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.faruk.wt_travel_agency;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +17,7 @@ import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //region Private Variables
     private Toolbar mainToolbar;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton addBtn;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private NavigationView mNavigationView;
 
 //endregion
 
@@ -36,10 +39,13 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         mainToolbar = (Toolbar) findViewById(R.id.navigation_actionbar);
+        mNavigationView = findViewById(R.id.navigation_view);
         setSupportActionBar(mainToolbar);
         getSupportActionBar().setTitle("WT Travel Agency");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         auth = FirebaseAuth.getInstance();
+
+        mNavigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -83,6 +89,25 @@ public class MainActivity extends AppCompatActivity {
     private void sendToLogin() {
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class); //prebacuje nas na login
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mDrawerLayout.closeDrawers();
+
+        switch (item.getItemId()){
+
+            case  R.id.log_out:
+                logOut();
+                return  true;
+
+            case R.id.account_settings:
+                sendToAccountSettings();
+                return  true;
+
+            default:
+                return  false;
+        }
     }
     //endregion
 
